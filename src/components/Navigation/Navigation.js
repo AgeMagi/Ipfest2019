c1b9d1db92ddf1a9c749082c4ab77e912c1b4d12
Habibi <muhhabibih@gmail.com>
2018-07-26 23:18:49 +0700

    Navigation sudah lumayan bagus

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,16 +7,38 @@ class Navigation extends Component {
     render() {
         return (  
             <div className='navbar'>
-                <Grid columns={3}>
+                <Grid columns={5}>
                     <Grid.Row>
                         <Grid.Column>
-                            <Image className='logo-navbar' src={require('../../images/Logo.png')} />
+                            <div className="navbar-link">
+                                <a> 
+                                    <h1>ABOUT</h1>
+                                </a>
+                            </div>
                         </Grid.Column>
                         <Grid.Column>
-                            <Image src={require('../../images/Logo.png')} />
+                            <div className="navbar-link">
+                                <a>
+                                    <h1>EVENTS</h1>
+                                </a>
+                            </div>
                         </Grid.Column>
                         <Grid.Column>
-                            <Image src={require('../../images/Logo.png')} />
+                            <Image size="small" src={require('../../images/Logo.png')} />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div className="navbar-link">
+                                <a>
+                                    <h1>REGISTRATION</h1>
+                                </a>
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div className="navbar-link">
+                                <a>
+                                    <h1>SPONSORS & MEDIA</h1>
+                                </a>
+                            </div>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
