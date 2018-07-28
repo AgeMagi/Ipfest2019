34a6d3adf400a7bb89084745b27d5cea4af4c582
Habibi <muhhabibih@gmail.com>
2018-07-28 17:00:40 +0700

    Masih kacau event-type

--- src/components/Event-Summary/Event_Summary.js
+++ src/components/Event-Summary/Event_Summary.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
 
+import { Container, Row, Col } from 'reactstrap';
+
+import './Event_Summary.css'
 export default class Event_Summary extends Component {
     render() {
         return(
             <div>
-                <img src={require("../")}/>
+                <Container className="event-summary-container">
+                    <Row>
+                        <Col sm="6"> 
+                            <h1>Tackling the Challenge</h1>
+                            <p>As the demand of energy increases, we are responsible 
+                                to provide the sustainibility of energy. In order to 
+                                aim it, we are supposed to think globally and visionary 
+                                along with our values in order to get the glory.</p>
+                        </Col>
+                        <Col sm="6">
+                            <Row>
+                                <Col sm="4" className="event-type"> 
+                                
+                                </Col>
+                                <Col sm="4" className="event-type"> 
+                                
+                                </Col>
+                                <Col sm="4" className="event-type"> 
+                                
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col sm="6" className="event-type"> 
+                                    <div className="event-type-image">
+
+                                    </div>
+                                </Col>
+                                <Col sm="6" className="event-type"> 
+                                
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+                {/* <Grid>
+                    <Grid.Row textAlign='center'>
+                        <Grid.Column width={6}>
+                            <h1>Tackling the Challenge</h1>
+                            <p>As the demand of energy increases, we are responsible 
+                                to provide the sustainibility of energy. In order to 
+                                aim it, we are supposed to think globally and visionary 
+                                along with our values in order to get the glory.</p>
+                        </Grid.Column>
+                        <Grid.Column className="event-type"width={2}>
+                            <img src={require("../../images/Synergistic.png")}/>
+                        </Grid.Column>
+                        <Grid.Column width={2}>
+                            HAII
+                        </Grid.Column>
+                        <Grid.Column width={2}>
+                            HAII
+                        </Grid.Column>
+                        <Grid.Column width={2}>
+                            HAII
+                        </Grid.Column>
+                        <Grid.Column width={2}>
+                            HAII
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid> */}
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'video-react/dist/video-react.css';
 
 import Navigation from './components/Navigation/Navigation';
 import Header from './components/Header/Header';
+import Event_Summary from './components/Event-Summary/Event_Summary'
 
 ReactDOM.render(
     <div>
         <Navigation />
         <Header />
+        <Event_Summary />
     </div>
     , 
     document.getElementById('root')
