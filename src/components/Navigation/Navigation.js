9c05dcfd2626ebd5ca8ec186ace7b4caff8179c2
Habibi <muhhabibih@gmail.com>
2018-07-28 14:58:18 +0700

    Finish Header

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,50 +1,71 @@
-import React, { Component } from 'react';
-import { Image, Grid} from 'semantic-ui-react';
+import React from 'react';
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem } from 'reactstrap';
 
-import './Navigation.css'
+import './Navigation.css';
 
-class Navigation extends Component {
-    render() {
-        return (  
-            <div className='navbar'>
-                <Grid columns={5}>
-                    <Grid.Row>
-                        <Grid.Column>
-                            <div className="navbar-link">
-                                <a> 
-                                    <h1>ABOUT</h1>
-                                </a>
-                            </div>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <div className="navbar-link">
-                                <a>
-                                    <h1>EVENTS</h1>
-                                </a>
-                            </div>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Image size="small" src={require('../../images/Logo.png')} />
-                        </Grid.Column>
-                        <Grid.Column>
-                            <div className="navbar-link">
-                                <a>
-                                    <h1>REGISTRATION</h1>
-                                </a>
-                            </div>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <div className="navbar-link">
-                                <a>
-                                    <h1>SPONSORS & MEDIA</h1>
-                                </a>
-                            </div>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </div>
-        );
-    };
+export default class Navigation extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+		isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+		isOpen: !this.state.isOpen
+		});
+	}
+	render() {
+		return (
+		<div>
+			<Navbar color="light" className="navbar" light expand="md">
+			<NavbarBrand href="/">
+				<img className="logo" src={require('../../images/Logo.png')}/>
+			</NavbarBrand>
+			<NavbarToggler onClick={this.toggle} />
+			<Collapse className="navbar-collapse" isOpen={this.state.isOpen} navbar>
+				<Nav className="ml-auto" navbar>
+					<NavItem>
+						<NavLink className="navbar-link" href="/components/">
+							<h1>ABOUT</h1>
+							<img src={require("../../images/Layer 1.png")} />
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink className="navbar-link" href="/components/">
+							<h1>EVENT</h1>
+							<img src={require("../../images/Layer 1.png")} />
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink className="navbar-link" href="/components/">
+							<h1>REGISTRATION</h1>
+							<img src={require("../../images/Layer 1.png")} />
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink className="navbar-link" href="/components/">
+							<h1>SPONSOR & MEDIA</h1>
+							<img src={require("../../images/Layer 1.png")} />
+						</NavLink>
+					</NavItem>
+				</Nav>
+			</Collapse>
+			</Navbar>
+		</div>
+		);
+	}
 }
-
-export default Navigation;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'video-react/dist/video-react.css';
 
 import Navigation from './components/Navigation/Navigation';
+import Header from './components/Header/Header';
 
 ReactDOM.render(
     <div>
         <Navigation />
+        <Header />
     </div>
     , 
     document.getElementById('root')
