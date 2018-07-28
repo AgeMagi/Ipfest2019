e334867f0467eb6039785d2276cd5eced59c62f9
Habibi <muhhabibih@gmail.com>
2018-07-28 15:22:59 +0700

    Delete yarn

---
+++ src/components/Event-Summary/Event_Summary.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Event_Summary extends Component {
+    render() {
+        return(
+            <div>
+                <img src={require("../")}/>
+            </div>
+        );
+    }
+}
