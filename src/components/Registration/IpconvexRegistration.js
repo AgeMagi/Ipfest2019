d3ac2e94ec4fe6133a3589f6b8a850aa4069a2a2
Habibi <muhhabibih@gmail.com>
2019-02-07 22:24:37 +0700

    Add foto

--- src/components/Registration/IpconvexRegistration.js
+++ src/components/Registration/IpconvexRegistration.js
@@ -46,7 +46,7 @@ const registration = [
                                 <h6>Djoko Siswanto</h6>
                             </Col>
                         </Row>
-                        <h5>Session 2 (Regulation Season)</h5>
+                        <h5>Session 2 (Technical Season)</h5>
                         <Row className='align-items-center'>
                             <Col sm={3} className='speaker-image-container'>
                                 <img 
@@ -57,6 +57,18 @@ const registration = [
                             <Col sm={9} className='speaker-content'>
                                 <h6>Chairman Society of Indonesian Petroleum Engineer </h6>
                                 <h6>Prof&#8228; Ir&#8228; Tutuka Ariadji M&#8228;Sc&#8228;,Ph&#8228;D</h6>
+                            </Col>
+                        </Row>
+                        <Row className='align-items-center'>
+                            <Col sm={3} className='speaker-image-container'>
+                                <img 
+                                    className='speaker-image'
+                                    src={require('../../images/IPCONVEX/foto-4.jpg')}
+                                />
+                            </Col>
+                            <Col sm={9} className='speaker-content'>
+                                <h6>Manager of Enhanced Oil Recovery Medco E&P Indonesia</h6>
+                                <h6>Puti Permata</h6>
                             </Col>
                         </Row>
                     </div>
