2891655e723a0f1f29adfe2e02b1ebbc649ddd6a
Habibi <muhhabibih@gmail.com>
2019-02-15 22:45:09 +0700

    Fix margin

--- src/components/MediaPartner/MediaPartner.js
+++ src/components/MediaPartner/MediaPartner.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 
+import './MediaPartner.css';
 export default class MediaPartner extends Component {
     render() {
         return(
             <div>
-                <div className="sponsors">
+                <div className="sponsors media-partner">
                     <img 
                         src={require('../../images/Sponsor/media-partner.png')}
                     />
